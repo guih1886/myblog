24883a064cc5fab6acee9d6a450c6d7573e126c4
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-01-11 11:31:26 -0300

    Projects update

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import '../CSS/Projects.css'
+import React from 'react';
+import '../CSS/Projects.css';
 
 export default function ProjectsPage() {
   return (
     <div className="content">
       <div className='projects'>
-        <p>ProjectsPage</p>
+        <p></p>
       </div>
     </div>
-  )
+  );
 }
