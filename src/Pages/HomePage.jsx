866b8f316cbc972d05cfe3b7c07c942c11dcabf6
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-01-11 11:31:10 -0300

    home update

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,15 +1,18 @@
-import React from 'react'
-import '../CSS/Home.css'
+import React from 'react';
+import '../CSS/Home.css';
 
 export default function HomePage() {
   return (
     <div className="content">
       <div className="home">
         <p className='greetings'>
-          <h1>Olá, Seja bem vindo ao meu site!</h1>
-          <h3>Aqui você poderá encontrar um pouco mais de informações sobre mim, meus projetos desenvolvidos, meus contatos além de ter disponível meu curriculo atualizado!</h3>
+          <h1>Olá, seja bem vindo ao meu site!</h1>
+          <h3>Aqui você poderá encontrar um pouco mais de informações sobre mim, meus projetos desenvolvidos, meus contatos além de ter disponível meu currículo atualizado!</h3>
         </p>
+        <div className="img">
+          <img src="https://i.pinimg.com/originals/21/11/61/21116158daaeb1459b4ec0758505e1ad.gif" alt="Programando" />
+        </div>
       </div>
     </div>
-  )
+  );
 }
