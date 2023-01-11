b864bc0ceddc0670f2587959b3a55241b7515459
Guilherme Henrique <guilherme_18henrique@yahoo.com.br>
2023-01-11 11:35:55 -0300

    contact update

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -10,16 +10,16 @@ export default function ContactPage() {
         <h1>Contatos</h1>
         <div className="l1">
           <a href='https://wa.me/5519982210064?text=Olá%20Guilherme,%20tudo%20bem?'
-            target='_blank'>
+            target='_blank' rel='noreferrer'>
             <ImWhatsapp size={80} color={'#25D366'} />
           </a>
-          <a href='https://www.linkedin.com/in/guih1886/' target='_blank'>
+          <a href='https://www.linkedin.com/in/guih1886/' target='_blank' rel='noreferrer'>
             <ImLinkedin size={80} color={'#0077b5'} />
           </a>
-          <a href='https://github.com/guih1886' target='_blank'>
+          <a href='https://github.com/guih1886' target='_blank' rel='noreferrer'>
             <ImGithub size={80} color={'#fff'} />
           </a>
-          <a href='mailto:guilherme_18henrique@yahoo.com.br' target='_blank'>
+          <a href='mailto:guilherme_18henrique@yahoo.com.br' target='_blank' rel='noreferrer'>
             <MdOutlineMail size={80} color={'#fff'} />
           </a>
         </div>
